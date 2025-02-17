d1046db088426fd4c6ef7c79402f42fb3d80c029
VienCat <vien303cat@gmail.com>
2025-02-17 16:48:44 +0800

    0217 upload

--- src/pages/store/[id].vue
+++ src/pages/store/[id].vue
@@ -2,7 +2,7 @@
   <v-container>
     <v-row>
       <v-col cols="12" color="primary">
-        <h1>123</h1>
+        <h1>幻想一下有swiper</h1>
       </v-col>
 
       <v-col cols="12" md="4" class="d-flex justify-center">
@@ -20,12 +20,16 @@
       <v-col cols="12" md="4" class="text-center">
         <h1>{{ store.name }}</h1>
         <h3>評價數:</h3>
-        <h3>綜合評價:**</h3>
+        <h3>
+          綜合評價:
+          <v-rating readonly length="1" size="18" model-value="1" active-color="info" />
+        </h3>
         <h3>{{ store.timetxt }}</h3>
         <pre>{{ store.depiction }}</pre>
         <h3>{{ store.adress }}</h3>
+        <br />
         <v-btn
-          v-if="user.isLoggedIn && 1"
+          v-if="user.isLoggedIn && !scroeDone"
           color="secondary"
           variant="flat"
           append-icon="mdi-noodles"
@@ -34,7 +38,7 @@
         </v-btn>
 
         <v-btn
-          v-else-if="user.isLoggedIn && 0"
+          v-else-if="user.isLoggedIn && scroeDone"
           color="info"
           variant="flat"
           append-icon="mdi-noodles"
@@ -45,28 +49,43 @@
     </v-row>
   </v-container>
 
-  <v-dialog v-model="dialog.open" persistent max-width="600px" max-height="80vh">
+  <v-dialog v-model="dialog.open" persistent max-width="40%" max-height="80vh">
     <v-form :disabled="isSubmitting" @submit.prevent="submit">
       <v-card>
-        <v-card-title
-          ><h2>{{ dialog.id ? '編輯評論' : '新增評論' }}</h2></v-card-title
-        >
+        <v-card-title class="dialog-title">
+          <v-row class="align-start">
+            <v-col cols="12" md="4" class="d-flex justify-start">
+              <v-img :src="store.image" height="200" width="200"></v-img>
+            </v-col>
+            <v-col cols="12" md="8" class="text-left wrap-text">
+              <v-col cols="12">
+                <h2>{{ store.name }}</h2>
+                <h3>{{ store.timetxt }}</h3>
+              </v-col>
+              <v-col cols="12">
+                <h4>{{ store.adress }}</h4>
+              </v-col>
+            </v-col>
+          </v-row>
+        </v-card-title>
         <v-card-text>
-          <v-text-field
-            v-model="name.value.value"
-            label="名稱"
-            :error-messages="name.errorMessage.value"
-          ></v-text-field>
-          <v-text-field
-            v-model="timetxt.value.value"
-            label="營業時間"
-            :error-messages="timetxt.errorMessage.value"
-          ></v-text-field>
-          <v-text-field
-            v-model="adress.value.value"
-            label="地址"
-            :error-messages="adress.errorMessage.value"
-          ></v-text-field>
+          <h2>{{ dialog.id ? '編輯評論' : '新增評論' }}</h2>
+          <br />
+
+          <!-- 顯示錯誤訊息 -->
+          <span v-if="star.errorMessage" class="text-secondary">{{ star.errorMessage }}</span>
+          <v-rating
+            v-model="star.value.value"
+            length="10"
+            :item-labels="['評分', '', '', '', '', '', '', '', '', '']"
+            active-color="info"
+            color="info"
+            size="24"
+            hover
+          ></v-rating>
+          <p>當前評分: {{ star.value.value }}</p>
+          <!-- 顯示當前評分 -->
+
           <v-textarea
             v-model="depiction.value.value"
             label="描述"
@@ -79,11 +98,9 @@
             accept="image/jpeg, image/png"
             deletable
             max-size="1MB"
-            help-text="請上傳店家圖片"
+            help-text="上傳圖片"
             :error-text="{ type: '檔案格式不符', size: '圖片容量過大' }"
           ></VueFileAgent>
-
-          <pre>{{ errors }}</pre>
         </v-card-text>
         <v-card-actions>
           <v-btn @click="closeDialog">取消</v-btn>
@@ -102,7 +119,6 @@ import { useUserStore } from '@/stores/user'
 import { useSnackbar } from 'vuetify-use-dialog'
 import { useForm, useField } from 'vee-validate'
 import * as yup from 'yup'
-// import Swal from 'sweetalert2'
 
 const user = useUserStore()
 const createSnackbar = useSnackbar()
@@ -117,59 +133,60 @@ const store = ref({
   timetxt: '',
   image: '',
   depiction: '',
+  star: '', // 確保這裡有 star 屬性
 })
 
 const getStore = async () => {
   try {
     const { data } = await apiAuth.get(`/store/` + route.params.id)
     store.value = data.result
-    console.log('取得店家資訊', store.value)
   } catch (error) {
     console.error('取得店家資訊失敗:', error)
   }
 }
 getStore()
 
-// TODO: 0214 回來開始做下面這兩個func資料還沒抓好(可能要判斷是沒資料 還是抓失敗了)
-// TODO: 上面HTML需要把form的資訊改一下 目前是改新增商店的資訊
-// TODO: 想法:首頁可以用swiper輪播圖 或是overlay來個廣告之類的 加油O_Q/
-
-const scores = reactive([])
+const scores = ref([])
 const getScores = async () => {
   try {
-    const { data } = await apiAuth.get(`/score/` + route.params.id)
-    scores.splice(0, scores.length)
-    scores.push(...data.result)
-    console.log('取得店家總評分', scores.value)
+    const { data } = await apiAuth.get('/score/getstore/' + route.params.id)
+    scores.value.push(...data.result)
   } catch (error) {
-    console.error('取得店家評分失敗:', error)
+    console.error('取得麵屋列表失敗:' + error)
+    createSnackbar({
+      text: error?.response?.data?.message || '未知錯誤',
+      snackbarProps: {
+        color: 'red',
+        timeout: 1000,
+      },
+    })
   }
 }
 getScores()
+console.log('scores:', scores.value)
 
-const myScore = reactive([])
+const myScore = ref({})
 const getScorebyUser = async () => {
   try {
-    const { data } = await apiAuth.get(`/getuser/${route.params.id}/${user.id}`)
-    console.log(data)
+    const { data } = await apiAuth.get(`score/getuser/${route.params.id}/${user.id}`)
+    // console.log(data)
     myScore.value = data.result
   } catch (error) {
     console.error('取得個人評分失敗:', error)
   }
 }
 getScorebyUser()
+console.log('myScore:', myScore.value)
 
 const dialog = ref({
   open: false,
-  // id判斷現在是新增還是編輯
   id: '',
 })
 const openDailog = (item) => {
+  console.log('open', myScore.value)
   if (item) {
     dialog.value.id = item._id
-    name.value.value = item.name
-    timetxt.value.value = item.timetxt
-    adress.value.value = item.adress
+    star.value.value = item.value
     depiction.value.value = item.depiction
   }
   dialog.value.open = true
@@ -177,61 +194,45 @@ const openDailog = (item) => {
 const closeDialog = () => {
   resetForm()
   dialog.value.id = ''
+  store.value.star = ''
   dialog.value.open = false
   fileAgent.value.deleteFileRecord()
 }
 
 const schema = yup.object({
-  name: yup.string().required('請輸入名稱'),
-  depiction: yup.string().required('請輸入描述'),
-  timetxt: yup.string().required('請輸入營業時間'),
-  adress: yup.string().required('請輸入地址'),
+  star: yup.number().required('請填寫評價').min(1, '評價不能為空'),
+  depiction: yup.string().required('請填寫描述'),
 })
 
-// 建立表單
 const { handleSubmit, isSubmitting, resetForm } = useForm({
   validationSchema: schema,
 })
-const name = useField('name')
+const star = useField('star')
 const depiction = useField('depiction')
-const timetxt = useField('timetxt')
-const adress = useField('adress')
 
-// 要清空 file agent 不能直接把 fileRecords 設為空(沒有用)因此利用 fileAgent 的方法來清空上傳的圖片(取消新增/編輯時)
 const fileAgent = ref(null)
 const fileRecords = ref([])
 const rawFileRecords = ref([])
 
 const submit = handleSubmit(async (values) => {
-  if ((fileRecords.value.length === 0 && dialog.value.id == '') || fileRecords.value[0]?.error) {
-    createSnackbar({
-      text: '店家圖片不可為空或是格式錯誤',
-      snackbarProps: {
-        color: 'red',
-        timeout: 2000,
-      },
-    })
-    return
-  }
+  console.log('submit', values)
   try {
     const formData = new FormData()
-    formData.append('name', values.name)
-    formData.append('timetxt', values.timetxt)
-    formData.append('adress', values.adress)
+    formData.append('user', user.id)
+    formData.append('store', store.value._id)
+    formData.append('star', values.star)
     formData.append('depiction', values.depiction)
 
-    // 編輯時若沒修改圖片則用原來的圖片
     if (fileRecords.value.length > 0) {
       formData.append('image', fileRecords.value[0].file)
     }
     if (dialog.value.id) {
-      await apiAuth.patch(`/store/` + dialog.value.id, formData)
+      await apiAuth.patch(`/score/` + dialog.value.id, formData)
     } else {
-      await apiAuth.post('/store', formData)
+      await apiAuth.post('/score', formData)
     }
 
-    //陣列全清空再重新抓一次全部資料 (若是資料量大建議抓id來更新或是直接提交內容來更新)
-    scores.splice(0, scores.length)
+    scores.value.splice(0, scores.value.length)
     getScores()
 
     createSnackbar({
@@ -245,7 +246,7 @@ const submit = handleSubmit(async (values) => {
   } catch (error) {
     console.error(error)
     createSnackbar({
-      text: '新增/編輯麵屋失敗 ',
+      text: '新增/編輯評價失敗 ',
       snackbarProps: {
         color: 'red',
         timeout: 2000,
@@ -254,6 +255,19 @@ const submit = handleSubmit(async (values) => {
   }
 })
 </script>
+
+<style scoped>
+.dialog-title {
+  margin-top: 20px; /* 添加上邊距 */
+}
+.text-left {
+  text-align: left; /* 文字靠左對齊 */
+}
+.wrap-text {
+  white-space: pre-wrap; /* 使文本換行 */
+  word-wrap: break-word; /* 使長單詞換行 */
+}
+</style>
 
 <route lang="yaml">
 meta:
