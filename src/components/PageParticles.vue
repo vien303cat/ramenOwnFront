78ec70566c0d0c5ed20e98617169fe8e374c6477
VienCat <vien303cat@gmail.com>
2025-02-27 15:34:01 +0800

    particle fix

--- src/components/PageParticles.vue
+++ src/components/PageParticles.vue
@@ -12,14 +12,3 @@ const particlesLoaded = async (container) => {
 
 const options = ref(particlesOptions)
 </script>
-<style scoped>
-.particles-container {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 0; /* 確保粒子背景不會遮擋其他內容 */
-  pointer-events: none;
-}
-</style>
