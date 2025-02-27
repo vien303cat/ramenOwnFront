d17a63128a397fe7bc5c06abcc5c97a10a4d5e97
vien303cat <39782979+vien303cat@users.noreply.github.com>
2025-02-27 06:01:56 +0000

    Deploying to gh-pages from @ vien303cat/ramenOwnFront@e92804ad099111ba3dbecff4086654a3773f5f03 🚀

---
+++ assets/ssrBoot-DTnI-1DK.js
@@ -0,0 +1,1 @@
+import{s,o as t,c as e,aM as n}from"./index-DS7cpca7.js";function i(){const o=s(!1);return t(()=>{window.requestAnimationFrame(()=>{o.value=!0})}),{ssrBootStyles:e(()=>o.value?void 0:{transition:"none !important"}),isBooted:n(o)}}export{i as u};
